b764e59dedfa48dc7b77d6f6096c8ebb361bf845
vilar <loadslowly@gmail.com>
2025-08-06 12:50:32 +0500

    init: initial setup

---
+++ src/test/java/itk/academy/wallet/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package itk.academy.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
